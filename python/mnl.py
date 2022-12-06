8a4dc268a969f0555c179cf29ac2e06afb2a4abd
MatthewDShen <matthewdshen@gmail.com>
2022-12-06 15:23:23 -0500

    Remove python notebook & setup init and venv
    Add link on how to do mnl as comment

---
+++ python/mnl.py
@@ -0,0 +1,11 @@
+import __init__
+
+
+# Read CSV with survey responses
+df_response = pd.read_csv(os.getcwd()[:-6] + 'input/Revised Copy of Mobility Project Survey (Responses).csv')
+print(df_response)
+
+
+# https://www.datasklr.com/logistic-regression/multinomial-logistic-regression
+
+
